6f2723a264388c72f02ca879166b00bbfec1c70f
Felipe França <felipefrancavieira5@gmail.com>
2024-06-14 12:29:45 -0300

    finished routes

--- openweatherapi/src/App.js
+++ openweatherapi/src/App.js
@@ -1,31 +1,9 @@
+import { Outlet } from "react-router";
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { getWeatherByCity } from './helpers/requests';
-import GalaxyNav from './components/GalaxyNav.js';
-import GalaxyMain from './components/GalaxyMain.js';
-
 function App() {
-  const [weatherData, setWeatherData] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getWeatherByCity('São Paulo');
-        setWeatherData(data);
-        console.log(data.temp);
-      } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-      }
-    }
-
-    fetchData();
-  }, []);
-
   return (
-    <div className="App">
-      <GalaxyNav/>
-      <GalaxyMain/>
-
+    <div className="app">
+      <Outlet/>
     </div>
   );
 }
--- openweatherapi/src/index.js
+++ openweatherapi/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from "./App";
+import Home from "./pages/home";
+import Weather from "./pages/weather";
+import Forecast from "./pages/forecast";
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/weather",
+        element: <Weather />,
+      },
+      {
+        path: "/forecast",
+        element: <Forecast />,
+      },
+    ],
+  },
+]);
+
+//renderizando o router
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
+
