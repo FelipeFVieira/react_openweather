0e76a7d76c6ee60db9b0d7d7b5b29040eea7c94a
Felipe França <felipefrancavieira5@gmail.com>
2024-06-14 12:28:26 -0300

    feat: finished helpers

--- openweatherapi/src/helpers/requests.js
+++ openweatherapi/src/helpers/requests.js
@@ -1,6 +1,5 @@
 import baseRequest from './axiosBaseUrl'
 
-
 export async function getWeatherByCity(city) {
     try {
         const response = await baseRequest.get('/weather', {
@@ -10,15 +9,94 @@ export async function getWeatherByCity(city) {
                 units:'metric'
             }
         });
+
+        const temp = Math.round(response.data.main.temp) 
+        const temp_min = Math.round(response.data.main.temp_min) 
+        const temp_max = Math.round(response.data.main.temp_max) 
+        const feels_like = Math.round(response.data.main.feels_like) 
         const dataObject = [
-            {temp: response.data.main.temp,
-            feels_like: response.data.main.feels_like,
-            humidity: response.data.main.humidity,
-            pressure: response.data.main.pressure}
+
+            {
+                temp: temp,
+                temp_min: temp_min,
+                temp_max: temp_max,
+                feels_like: feels_like,
+                humidity: response.data.main.humidity,
+                pressure: response.data.main.pressure,
+                wind_speed: response.data.wind.speed,
+                wind_deg: response.data.wind.deg,
+                name: response.data.weather[0].main,
+
+            }
 
         ]
         return dataObject
         
+    } catch (error) {
+        console.error('Erro ao fazer requisição:', error);
+        throw error;
+    }
+}
+
+
+
+
+export async function getForecastByCity(city) {
+    try {
+        const response = await baseRequest.get('/forecast', {
+            params: {
+                q: city, 
+                appid: 'a1aac202a64668472fff5a3689bc4c61',
+                units:'metric'
+            }
+        });
+
+    const today = new Date().toISOString().split('T')[0];
+
+    const dataObject = [];
+    let lastDateOnly;
+    response.data.list.forEach(forecast => {
+
+      const dateOnly = forecast.dt_txt.split(' ')[0]; 
+
+      if(dateOnly != lastDateOnly && dateOnly != today){
+        lastDateOnly = dateOnly
+        dataObject.push({
+            temp_min: forecast.main.temp_min,
+            temp_max: forecast.main.temp_max,
+            humidity: forecast.main.humidity,
+            pressure: forecast.main.pressure,
+            wind_speed: forecast.wind.speed,
+            wind_deg: forecast.wind.deg, 
+            name: forecast.weather[0].main,
+            date: dateOnly
+
+        })
+      }
+    });
+  
+    // const temp = Math.round(response.data.main.temp) 
+    // const temp_min = Math.round(response.data.main.temp_min) 
+    // const temp_max = Math.round(response.data.main.temp_max) 
+    // const feels_like = Math.round(response.data.main.feels_like) 
+    // dataObject = [
+
+    //     {
+    //         temp: temp,
+    //         temp_min: temp_min,
+    //         temp_max: temp_max,
+    //         feels_like: feels_like,
+    //         humidity: response.data.main.humidity,
+    //         pressure: response.data.main.pressure,
+    //         wind_speed: response.data.wind.speed,
+    //         wind_deg: response.data.wind.deg,
+    //         name: response.data.weather[0].main,
+
+    //     }
+
+    // ]
+    return dataObject
+    
     } catch (error) {
         console.error('Erro ao fazer requisição:', error);
         throw error;
